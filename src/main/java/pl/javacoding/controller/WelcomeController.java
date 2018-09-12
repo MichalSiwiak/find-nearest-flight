5c942b4c560255a28ae955b84a187551dd185a96
siwiakmichal <siwiakmichal@gmail.com>
2018-09-13 01:01:28 +0200

    addin bootstrap frond-end

--- src/main/java/pl/javacoding/controller/WelcomeController.java
+++ src/main/java/pl/javacoding/controller/WelcomeController.java
@@ -30,11 +30,6 @@ public class WelcomeController {
     @Value("${welcome.message}")
     private String message = "Hello World";
 
-    @RequestMapping("/")
-    public String welcome(Map<String, Object> model) {
-        model.put("message", this.message);
-        return "welcome";
-    }
 
     @RequestMapping(value = "/flight", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
     public ResponseEntity<?> findNearestFlight() throws MalformedURLException {
